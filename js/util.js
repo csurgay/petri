9a3fb8db8cd801b02d0eb425ea35daca065c66fe
Peter Csurgay <csurgay@gmail.com>
2024-10-03 14:58:42 +0200

    old help back and firefox support from IV9VBS

--- js/util.js
+++ js/util.js
@@ -1,13 +1,28 @@
+/// IV9VBS Rework -> Open in multiple Browsers!
 function here() {
   const e = new Error();
-  const regex = /\((.*):(\d+):(\d+)\)$/
-  const match = regex.exec(e.stack.split("\n")[2]);
-  var filepath = match[1];
-  var line = match[2];
-  var column = match[3];
-  var filename = filepath.replace(/^.*[\\/]/, '')
-  return filename+"("+line+"): "
+  const stackLine = e.stack ? e.stack.split("\n")[2] : null;
+
+  if (stackLine) {
+      const regex = /@?(.*):(\d+):(\d+)/;
+      const match = regex.exec(stackLine);
+
+      if (match) {
+          var filepath = match[1];
+          var line = match[2];
+          var column = match[3];
+          var filename = filepath.replace(/^.*[\\/]/, '');
+          return filename + "(" + line + "): ";
+      } else {
+          console.warn("Stack trace format did not match expected pattern:", stackLine);
+          return "unknown location: ";
+      }
+  } else {
+      console.error("Could not retrieve stack trace.");
+      return "unknown location: ";
+  }
 }
+  
 function log(here="js: ", str) {
     console.log(here+str);
 }
@@ -17,12 +32,14 @@ function error(here="js: ", str) {
 
 var idPlace=0, idTrans=0, idFlow=0, idLabel=0, idMidpoint=0;
 function nextId(type) {
-    if (type=="PLACE") return ++idPlace;
-    else if (type=="TRANSITION") return ++idTrans;
-    else if (type=="FLOW") return ++idFlow;
-    else if (type=="LABEL") return ++idLabel;
-    else if (type=="MIDPOINT") return ++idMidpoint;
-    else error(here(), "Unknown oject type: "+type);
+    switch (type) {
+      case "PLACE": return ++idPlace;
+      case "TRANSITION": return ++idTrans;
+      case "FLOW": return ++idFlow;
+      case "LABEL": return ++idLabel;
+      case "MIDPOINT": return ++idMidpoint;
+      default: error(here(), "Unknown object type: " + type);
+    }
 }
 function rotate(cx,cy,x,y,alpha) {
     var tx=x-cx,ty=y-cy;
